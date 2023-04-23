ba8daae4eca4ca3bc9892ab7ccd8c1644ebdab60
npham49 <brian.d.pham@gov.bc.ca>
2023-04-22 19:10:45 -0700

    added prisma functionality for db operations

--- packages/backend/index.ts
+++ packages/backend/index.ts
@@ -1,9 +1,36 @@
-import fastify from "fastify";
+import fastify, {
+  FastifyInstance,
+  FastifyRequest,
+  FastifyReply,
+} from "fastify";
+import "@fastify/formbody";
+import { PrismaClient } from "@prisma/client";
 
-const server = fastify();
+const prisma = new PrismaClient();
+const server = fastify({ logger: true });
+
+server.register(require("@fastify/formbody"));
+
+// Or you can type your request using
+type CustomRequest = FastifyRequest<{
+  Body: { name: string; description: string; userId: string };
+}>;
 
 server.get("/ping", async (request, reply) => {
   return "pong\n";
+});
+
+server.post("/style", async (request: CustomRequest, reply: FastifyReply) => {
+  const { name, description, userId } = request.body;
+  const style = await prisma.style.create({
+    data: {
+      name,
+      description,
+      userId,
+    },
+  });
+  console.log(request.body);
+  reply.status(200).send({ addedItem: style, status: "done" });
 });
 
 server.listen({ port: 8080 }, (err, address) => {

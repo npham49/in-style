6eacab7efe1172950392ac265ccbfabbad343331
npham49 <nguyenphamswork@gmail.com>
2023-04-26 21:42:46 -0700

    added comments to main index from API

--- packages/backend/index.ts
+++ packages/backend/index.ts
@@ -3,10 +3,13 @@ import "@fastify/formbody";
 
 const server = fastify({ logger: true });
 
+//API middlewares
 server.register(require("@fastify/formbody"));
 
+//API routes
 server.register(require("./routes/styles.route"), { prefix: "styles" });
 
+//starts a server on the provided port
 server.listen({ port: 8080 }, (err, address) => {
   if (err) {
     console.error(err);
